ec9516c3c083f537d1dd1d4c565253490d45536b
Vitor Carvalho Matos <vitorcarvalhomatos@gmail.com>
2022-09-19 11:19:38 -0300

    removendo funções que não estão sendo usadas

--- src/repositories/testRepositories.ts
+++ src/repositories/testRepositories.ts
@@ -4,28 +4,3 @@ import { ITestsData } from "../types/testsTypes";
 export async function insert(testsData: ITestsData) {
     await prisma.tests.create({ data: testsData })
 }
-
-export async function getAllByTeacherId(teacherId: number) {
-    const result = await prisma.tests.findMany({
-        where: {
-            TeacherDisciplines: {
-                teacherId: teacherId
-            }
-        }, select: {
-            id: true,
-            name: true,
-            pdfUrl: true,
-            TeacherDisciplines: {
-                select: {
-                    Disciplines: {
-                        select: {
-                            name: true
-                        }
-                    }
-                }
-            }
-        }
-    });
-
-    return result;
-}
